359a1d563819e7c457bf9159d6b618bf86a6e88f
Amanda Meuser <ameuser@uoguelph.ca>
2023-06-27 09:16:11 -0700

    Added more fish

--- fishtree_script.R
+++ fishtree_script.R
@@ -26,3 +26,23 @@ pdf("fishtree_phylogeny.pdf", width = 5, height = 4)
 plot(phy, use.edge.length = T, tip.color = mycol)
 dev.off()
 
+#-----------------------------------------------------------
+# add more fish
+
+# load desired species into data object
+phy_more <- fishtree_phylogeny(species = c("Danio rerio", "Semotilus atromaculatus", "Pimephales promelas", "Carassius auratus", "Oncorhynchus mykiss", "Gasterosteus aculeatus", "Ctenopharyngodon idella"))
+phy_more
+
+# create an object to colour the species names
+names_more <- factor(c("out", "out", "in", "in", "out", "out", "in"))
+mycol_more <- c("firebrick2", "black")[names_more]
+
+#plot phylogeny
+plot(phy_more, use.edge.length = T, tip.color = mycol_more)
+
+
+# save as a pdf
+pdf("fishtree_phylogeny_morefish.pdf", width = 5, height = 4)
+plot(phy_more, use.edge.length = T, tip.color = mycol_more)
+dev.off()
+
